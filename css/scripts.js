f5be7f78a11e35672162a3c318843aeac190bae5
Isaac <Isaacyip@users.noreply.github.com>
2020-12-05 15:41:23 +0800

    1

--- css/scripts.js
+++ css/scripts.js
@@ -1,20 +1,12 @@
-$(function() {
-
-  var edit = document.getElementById('edit');
-
-  $(edit).blur(function(){
-    localStorage.setItem('todoData', this.innerHTML);
-  });
-
-  //when the page loads
-  if (localStorage.getItem('todoData')){
-    edit.innerHTML = localStorage.getItem('todoData');
+function clickCounter() {
+  if (typeof(Storage) !== "undefined") {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount)+1;
+    } else {
+      localStorage.clickcount = 1;
+    }
+    document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+  } else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
   }
-});
-
-
-if (typeof(Storage) !== "undefined") {
-  // Code for localStorage/sessionStorage.
-} else {
-  // Sorry! No Web Storage support..
 }
