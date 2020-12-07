63168775fa25a0cec5ec0f8ee5e1b911b4012cb0
Isaac <Isaacyip@users.noreply.github.com>
2020-12-07 21:02:27 +0800

    1

--- css/scripts.js
+++ css/scripts.js
@@ -1,12 +1,42 @@
-function clickCounter() {
-  if (typeof(Storage) !== "undefined") {
-    if (localStorage.clickcount) {
-      localStorage.clickcount = Number(localStorage.clickcount)+1;
-    } else {
-      localStorage.clickcount = 1;
-    }
-    document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
-  } else {
-    document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
-  }
+const strips = [...document.querySelectorAll(".strip")];
+const numberSize = "8"; // in rem
+
+// highlight number i on strip s for 1 second
+function highlight(strip, d) {
+  strips[strip]
+    .querySelector(`.number:nth-of-type(${d + 1})`)
+    .classList.add("pop");
+
+  setTimeout(() => {
+    strips[strip]
+      .querySelector(`.number:nth-of-type(${d + 1})`)
+      .classList.remove("pop");
+  }, 950); // causes ticking
 }
+
+function stripSlider(strip, number) {
+  let d1 = Math.floor(number / 10);
+  let d2 = number % 10;
+
+  strips[strip].style.transform = `translateY(${d1 * -numberSize}vmin)`;
+  highlight(strip, d1);
+  strips[strip + 1].style.transform = `translateY(${d2 * -numberSize}vmin)`;
+  highlight(strip + 1, d2);
+}
+
+setInterval(() => {
+  // get new time
+  const time = new Date();
+
+  // get h,m,s
+  const hours = time.getHours();
+  const mins = time.getMinutes();
+  const secs = time.getSeconds();
+
+  // slide strips
+  stripSlider(0, hours);
+  stripSlider(2, mins);
+  stripSlider(4, secs);
+
+  // highlight numbers
+}, 1000);
